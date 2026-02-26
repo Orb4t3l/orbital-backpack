20c0e8a89d642ad8d79326d1d44163f8d0924212
Orb4t3l <trevducket@gmail.com>
2026-02-25 18:09:31 -0800

    added moditems tab and got item generation working possibly

--- src/main/java/com/orbital/orbitalbackpack/registries/ModCreativeTabs.java
+++ src/main/java/com/orbital/orbitalbackpack/registries/ModCreativeTabs.java
@@ -1,4 +1,26 @@
 package com.orbital.orbitalbackpack.registries;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+import static com.orbital.orbitalbackpack.OrbitalBackpack.MODID;
+
 public class ModCreativeTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final RegistryObject<CreativeModeTab> BACKPACK = CREATIVE_MODE_TABS.register("backpack_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> ModItems.BACKPACK.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(ModItems.BACKPACK.get());
+//                output.accept(ModItems.BACKPACK.get()); other items if i want
+//                output.accept(ModItems.BACKPACK.get());
+//                output.accept(ModItems.BACKPACK.get());
+//                output.accept(ModItems.BACKPACK.get());
+            }).build());
 }
+
